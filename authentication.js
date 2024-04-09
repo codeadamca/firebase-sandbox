c2c456370098f278246e5d01c640119acafb80d7
Adam <thomasadam83@hotmail.com>
2024-04-09 12:47:25 -0400

    Added auth sample

--- authentication.js
+++ authentication.js
@@ -1,7 +1,3 @@
-// Documentation
-// https://firebase.google.com/docs/database/web/read-and-write
-
-// Import the functions you need from the SDKs you need
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import {
   getAuth,
@@ -10,9 +6,6 @@ import {
   signOut,
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -28,24 +21,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Authentication and get a reference to the service
 const auth = getAuth();
-
-// Setup the Google Auth Provider
 const provider = new GoogleAuthProvider();
 
-// Create references to each HTML element
 const signInButton = document.getElementById("signInButton");
 const signOutButton = document.getElementById("signOutButton");
 const message = document.getElementById("message");
 const userName = document.getElementById("userName");
 const userEmail = document.getElementById("userEmail");
 
-// Hide sign out and message until logged in
 signOutButton.style.display = "none";
 message.style.display = "none";
 
-// When sign in button is clicked
 const userSignIn = async () => {
   signInWithPopup(auth, provider)
     .then((result) => {
@@ -53,23 +40,22 @@ const userSignIn = async () => {
       console.log(user);
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
+      console.log(error.code);
+      console.log(error.message);
     });
 };
 
-// When sign out button is clicked
 const userSignOut = async () => {
   signOut(auth)
     .then(() => {
-      alert("You have signed out successfully!");
+      alert("You have been signed out!");
     })
-    .catch((error) => {});
+    .catch((error) => {
+      console.log(error.code);
+      console.log(error.message);
+    });
 };
 
-// When auth state chages
-// This is executed when the login popup is completed
-// Or the page loads and a user is logged in
 onAuthStateChanged(auth, (user) => {
   if (user) {
     signOutButton.style.display = "block";
@@ -82,6 +68,5 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-// Add event listeners
 signInButton.addEventListener("click", userSignIn);
 signOutButton.addEventListener("click", userSignOut);
