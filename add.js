f0a7d2b41628f6cba12769f76225764a3ba2da32
Adam <thomasadam83@hotmail.com>
2024-05-29 11:03:25 -0400

    Added add form

---
+++ add.js
@@ -0,0 +1,84 @@
+// Documentation
+// https://firebase.google.com/docs/database/web/read-and-write
+
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  child,
+  get,
+  onValue,
+  set,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase();
+
+// Fetch messages
+const messages = ref(database, "/messages");
+
+// On data event
+onValue(
+  messages,
+  (snapshot) => {
+    // Create a reference to the ul element
+    const ul = document.getElementById("messages");
+
+    // Empty the ul emelemt
+    ul.replaceChildren();
+
+    // Loop through messages
+    snapshot.forEach((childSnapshot) => {
+      // Get key and children
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+
+      console.log(childKey);
+      console.log(childData);
+
+      // Add message to list
+      const text = document.createTextNode(
+        childData.message + " ~ " + childData.name
+      );
+      const li = document.createElement("li");
+      li.appendChild(text);
+      ul.appendChild(li);
+    });
+  },
+  {
+    onlyOnce: false,
+  }
+);
+
+// Add event to button
+const submit = document.getElementById("submit");
+const name = document.getElementById("name");
+const message = document.getElementById("message");
+
+submit.addEventListener("click", function () {
+  const messages = ref(database, "messages");
+  push(messages, {
+    message: message.value,
+    name: name.value,
+    createdAt: new Date().getTime(),
+  });
+});
